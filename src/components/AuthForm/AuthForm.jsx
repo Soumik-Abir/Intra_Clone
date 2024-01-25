c1dfdfd8cfa0e6c5055a23923d2f5cc757adc032
Soumik Mukherjee <mukherjeesoumik700@gmail.com>
2024-01-26 01:21:55 +0530

    Add new files and components

---
+++ src/components/AuthForm/AuthForm.jsx
@@ -0,0 +1,26 @@
+import { Box, Flex } from "@chakra-ui/react";
+import React, { useState } from "react";
+
+const AuthForm = () => {
+    const [isLogin, setIsLogin] = useState(true);
+  return (
+    <>
+      <Box border={"1px solid gray"} borderRadius={4} padding={5}>
+        <Flex alignItems={"center"} justifyContent={"center"}>
+          <Box mx={2} fontSize={14}>
+            {isLogin ? "Don't have an account?" : "Already have an account?"}
+          </Box>
+          <Box
+            onClick={() => setIsLogin(!isLogin)}
+            color={"blue.500"}
+            cursor={"pointer"}
+          >
+            {isLogin ? "Sign up" : "Log in"}
+          </Box>
+        </Flex>
+      </Box>
+    </>
+  );
+};
+
+export default AuthForm;
